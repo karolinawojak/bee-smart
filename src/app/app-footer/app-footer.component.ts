3a20e5c7d9ec3a38b0fdd4cbc166e7891e546ea0
Lukasz <karolina.wojak@yahoo.com>
2019-12-14 08:10:33 +0100

    Added footer structure

---
+++ src/app/app-footer/app-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './app-footer.component.html',
+  styleUrls: ['./app-footer.component.scss']
+})
+
+export class AppFooterComponent {
+
+}
