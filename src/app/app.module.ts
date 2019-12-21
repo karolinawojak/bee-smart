93752c790c93b27ecbc9f5655ceadfc2c6629856
Lukasz <karolina.wojak@yahoo.com>
2019-12-21 08:35:14 +0100

    Structure and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppMainComponent } from './app-main/app-main.component';
+import { AppLastComponent } from './app-last/app-last.component';
+import localePl from '@angular/common/locales/pl';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePl);
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
-    AppMainComponent
+    AppMainComponent,
+    AppLastComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pl'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
