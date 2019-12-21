3b7786e9d973968b247be07d87a698d369a99c49
Lukasz <karolina.wojak@yahoo.com>
2019-12-21 08:07:33 +0100

    Added main component

---
+++ src/app/app-main/app-main.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './app-main.component.html',
+  styleUrls: ['./app-main.component.scss']
+})
+
+export class AppMainComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
