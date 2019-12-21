45f122a4f16057b8d4ce0b7de5a74c908aabbadd
Lukasz <karolina.wojak@yahoo.com>
2019-12-21 06:55:08 +0100

    Fixed selector

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-footer',
   templateUrl: './app-footer.component.html',
   styleUrls: ['./app-footer.component.scss']
 })
