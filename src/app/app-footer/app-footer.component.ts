5de650bb8de8318d60b83fbe6c76b4992b43b303
Lukasz <karolina.wojak@yahoo.com>
2019-12-21 06:28:51 +0100

    Added OnInit to components

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-footer.component.scss']
 })
 
-export class AppFooterComponent {
+export class AppFooterComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit() { }
 }
--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-header.component.scss']
 })
 
-export class AppHeaderComponent {
+export class AppHeaderComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit() { }
 }
