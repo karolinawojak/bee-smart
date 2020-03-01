bf9f45088da4af69abedafb87fbc3b8db77761ab
Lukasz <karolina.wojak@yahoo.com>
2020-03-01 18:47:28 +0100

    Cleanup

--- src/app/personal.service.ts
+++ src/app/personal.service.ts
@@ -6,14 +6,6 @@ import { User } from './user.model';
 })
 export class PersonalService {
 
-  private lastVisitDate: Date = new Date(2019, 11, 17, 3, 24, 0);
-  private firstVisitDate: Date = new Date(2019, 3, 5, 15, 35, 0);
-
-  private hiveArray: Array<{id: string, date: Date}> = [
-    {id: '1', date: new Date(2019, 3, 5, 15, 35, 0)},
-    {id: '2', date: new Date(2019, 7, 10, 3, 24, 0)},
-  ];
-
   private userData: User[] = [
     { userID: 'f4wfr',
     firstVisit: new Date(2019, 3, 5, 15, 35, 0),
