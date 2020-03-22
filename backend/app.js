3ee9b80b01526c9160b2e9b6aab486a49a4568e7
Lukasz <karolina.wojak@yahoo.com>
2020-03-22 10:12:24 +0100

    Cleanup

--- backend/app.js
+++ backend/app.js
@@ -26,16 +26,7 @@ expressApp.use((request, response, next) => {
   next();
 });
 
-expressApp.use('/api/personal', (request, response, next) => {
-  // const personalData = [
-  //   { id: 'f4wfr',
-  //   firstVisit: new Date('2019-04-09T18:04'),
-  //   lastVisit: new Date('2019-06-11T14:23'),
-  //   hives: [
-  //     { hiveID: 'g4ssxt', firstReading: new Date('2019-04-09T18:04') },
-  //     { hiveID: '2f45us', firstReading: new Date('2019-05-27T11:43') }
-  //   ]}
-  // ];
+expressApp.get('/api/personal', (request, response, next) => {
   User.find()
     .then(documents => {
       response.status(200).json(documents);
