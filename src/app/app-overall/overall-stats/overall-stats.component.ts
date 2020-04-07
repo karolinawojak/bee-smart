096e9aad3966ff9d00f88ac7e47e6d25bfe80ab1
Lukasz <karolina.wojak@yahoo.com>
2020-04-07 10:10:37 +0200

    Putting api data to chart

--- src/app/app-overall/overall-stats/overall-stats.component.ts
+++ src/app/app-overall/overall-stats/overall-stats.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HiveData } from '../../hiveData.model';
-import { MainStatsService } from '../../main-stats.service';
+import { ChartMonthService } from '../../chart-month.service';
 import { ChartsModule } from 'ng2-charts';
 import { Subscription } from 'rxjs';
 
@@ -12,36 +12,28 @@ import { Subscription } from 'rxjs';
 
 export class OverallStatsComponent implements OnInit {
 
-  statList: HiveData[];
+  statList = [];
   private statSubscription: Subscription;
 
   public lineChartOptions = {
     scaleShowVerticalLines: true,
     responsive: true,
     fill: false
-  };  public lineChartLabels = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj',
-  'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
+  };
+  public lineChartLabels = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
   public lineChartType = 'line';
-  public lineChartLegend = true;  public lineChartData = [
-    {fill: false, data: [35, 35, 35, 36, 37, 36, 35, 35, 35, 35, 36, 37,
-      36, 35, 35, 35, 35, 36, 37, 36, 35, 35, 35, 35], label: 'Temperatura'},
-    {fill: false, data: [75, 75, 74, 73, 72, 70, 70, 75, 75, 74, 73, 72,
-    72, 72, 75, 75, 74, 73, 72, 72, 72, 75, 75, 76], label: 'Wilgotność'},
-    {fill: false, data: [40, 36, 43, 49, 40, 48, 47, 42, 40, 41, 40, 36,
-    43, 59, 42, 40, 41, 40, 36, 43, 59, 42, 40, 41], label: 'Akustyka'},
-    {fill: false, borderColor: 'gray', pointBackgroundColor: 'gray',
-    data: [80, 90, 92, 90, 90, 90, 92, 80, 90, 92, 90, 90,
-    90, 92, 84, 85, 88, 90, 92, 90, 90, 90, 92, 91], label: 'Dwutlenek węgla'}
-  ];
+  public lineChartLegend = true;
+  public lineChartData =  [];
 
   // tslint:disable-next-line: variable-name
-  constructor(public statsService: MainStatsService) {}
+  constructor(public statsService: ChartMonthService) {}
 
   ngOnInit() {
-    this.statsService.getStats();
-    this.statSubscription = this.statsService.statsUpdateListener()
-      .subscribe((stats: HiveData[]) => {
-        this.statList = stats.slice(-2);
+    this.statsService.getMonthlyData();
+    this.statSubscription = this.statsService.monthlyStatsUpdateListener()
+      .subscribe((stats) => {
+        this.statList = stats;
       });
   }
 }
--- src/app/chart-month.service.ts
+++ src/app/chart-month.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HiveData } from './hiveData.model';
+import { User } from './user.model';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
@@ -8,21 +9,107 @@ import { Subject } from 'rxjs';
 })
 export class ChartMonthService {
 
+  private userData: User[] = [];
+  private hives = [];
   private statList: HiveData[] = [];
-  private statListUpdated = new Subject<HiveData[]>();
+  private temperatureList = [];
+  private humidityList = [];
+  private acousticsList = [];
+  private carbonDioxideList = [];
+  private minTemperature: number;
+  private maxTemperature: number;
+  private minHumidity: number;
+  private maxHumidity: number;
+  private minAcoustics: number;
+  private maxAcoustics: number;
+  private minCarbonDioxide: number;
+  private maxCarbonDioxide: number;
+  private chartList = [];
+  private chartData = [];
+  private chartDataUpdated = new Subject<HiveData[]>();
 
   // tslint:disable-next-line: variable-name
   constructor(private _http: HttpClient) { }
 
   getMonthlyData() {
+    let temperature: number;
+    let humidity: number;
+    let acoustics: number;
+    let carbonDioxide: number;
+
+    this._http.get<User[]>('http://localhost:3000/api/personal')
+      .subscribe((personalData) => {
+        this.userData = personalData;
+
+        // tslint:disable-next-line: prefer-for-of
+        for (let i = 0; i < personalData[0].hives.length; i++) {
+        this.hives.push(personalData[0].hives[i].hiveID);
+        }
+    });
+
     this._http.get<HiveData[]>('http://localhost:3000/api/monthly-stats')
       .subscribe((hiveData) => {
         this.statList = hiveData;
-        this.statListUpdated.next([...this.statList]);
+
+        for (let hive of this.hives) {
+          // tslint:disable-next-line: prefer-for-of
+          for (let i = 0; i < this.statList.length; i++) {
+            if (hive == this.statList[i].hiveID) {
+              temperature = this.statList[i].temperature;
+              humidity = this.statList[i].humidity;
+              acoustics = this.statList[i].acoustics;
+              carbonDioxide = this.statList[i].carbonDioxide;
+
+              this.temperatureList.push(temperature);
+              this.humidityList.push(humidity);
+              this.acousticsList.push(acoustics);
+              this.carbonDioxideList.push(carbonDioxide / 10);
+
+              this.minTemperature = Math.min.apply(null, this.temperatureList);
+              this.maxTemperature = Math.max.apply(null, this.temperatureList);
+              this.minHumidity = Math.min.apply(null, this.humidityList);
+              this.maxHumidity = Math.max.apply(null, this.humidityList);
+              this.minAcoustics = Math.min.apply(null, this.acousticsList);
+              this.maxAcoustics = Math.max.apply(null, this.acousticsList);
+              this.minCarbonDioxide = Math.min.apply(null, this.carbonDioxideList) * 10;
+              this.maxCarbonDioxide = Math.max.apply(null, this.carbonDioxideList) * 10;
+
+            }
+          }
+          this.chartList.push(this.temperatureList);
+          this.chartList.push(this.humidityList);
+          this.chartList.push(this.acousticsList);
+          this.chartList.push(this.carbonDioxideList);
+
+          this.chartData.push({hiveID: hive,
+            chartData:  [
+            {fill: false, data: this.temperatureList, label: 'Temperatura'},
+            {fill: false, data: this.humidityList, label: 'Wilgotność'},
+            {fill: false, data: this.acousticsList, label: 'Akustyka'},
+            {fill: false, borderColor: 'gray', pointBackgroundColor: 'gray',
+            data: this.carbonDioxideList, label: 'Dwutlenek węgla'}
+            ],
+          minTemp: this.minTemperature,
+          maxTemp: this.maxTemperature,
+          minHum: this.minHumidity,
+          maxHum: this.maxHumidity,
+          minAcous: this.minAcoustics,
+          maxAcous: this.maxAcoustics,
+          minCarbon: this.minCarbonDioxide,
+          maxCarbon: this.maxCarbonDioxide
+          });
+
+          this.temperatureList = [];
+          this.humidityList = [];
+          this.acousticsList = [];
+          this.carbonDioxideList = [];
+          this.chartList = [];
+        }
+        this.chartDataUpdated.next([...this.chartData]);
       });
   }
 
   monthlyStatsUpdateListener() {
-    return this.statListUpdated.asObservable();
+    return this.chartDataUpdated.asObservable();
   }
 }
